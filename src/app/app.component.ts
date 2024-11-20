bd2bbe4fff1f92c4139f6fb9144f9f103b8556a7
Fabio Porto <fabio.lmp@gmail.com>
2024-11-20 17:13:06 -0300

    Edita um herói

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HeroesComponent } from "./heroes/heroes.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    HeroesComponent,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+import { Hero } from '../hero.model';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [UpperCasePipe, FormsModule],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.scss'
+})
+export class HeroesComponent {
+
+  hero: Hero = {
+    id: 1,
+    name: "Wolverine"
+  };
+
+}
